a8b8c29c20c824c1ecce887148fe87c28ea0fd44
David Wilhelm <davwilh@gmail.com>
2020-10-22 01:33:32 -0400

    add auth methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  userInfo: any;
 
-  constructor() { }
+  constructor(
+    public firestore: AngularFirestore,
+    public fireAuth: AngularFireAuth,
+    public router: Router,
+  ) { }
+
+  async signIn(email: string, password: string): Promise<any> {
+    try {
+      const result = await this.fireAuth.signInWithEmailAndPassword(email, password);
+      // this.router.navigate(['']);
+      console.log(result);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  async signUp(email: string, password: string): Promise<any> {
+    try {
+      const result = await this.fireAuth.createUserWithEmailAndPassword(email, password);
+      console.log(result);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  async forgotPassword(email: string): Promise<any> {
+    try {
+      const result = this.fireAuth.sendPasswordResetEmail(email);
+      console.log(result);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  async signOut(): Promise<any> {
+    try {
+      this.fireAuth.signOut();
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
 }
