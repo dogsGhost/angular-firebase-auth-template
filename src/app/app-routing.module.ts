6569879ac88a2d8d46ec0cd1c2c09e6017a30f91
David Wilhelm <davwilh@gmail.com>
2020-10-26 15:53:40 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
-
+import { redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
 
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { HomeComponent } from './home/home.component';
 
-const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['sign-in']);
 const redirectLoggedInToHome = () => redirectLoggedInTo(['']);
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    // ...canActivate(redirectUnauthorizedToLogin),
   },
   {
     path: 'sign-up',
